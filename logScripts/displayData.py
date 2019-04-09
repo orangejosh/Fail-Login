7c8a7fe5bbf913569e8c1e2fd28bdd712ae8d402
Josh <josh@Joshs-iMac.local>
2019-04-09 12:49:28 -0500

    Can now map the bots. Systems trying to log in with the same username within a couple of minuites of each other are assumed to be controlled by the same system. displayData.py can now display those assumed bot nets

--- logScripts/displayData.py
+++ logScripts/displayData.py
@@ -5,34 +5,15 @@ from sqlite3 import Error
 args = sys.argv
 
 def main():
-	# Assumes a database has ben created
     conn = create_connection("/home/pi/logData/logData.db")
 
     with conn:
-		# No arguments returns a grouped list of the usernames used to login
-        if len(args) == 1:
-            sql = ''' SELECT username, COUNT(username) FROM logs 
-                        JOIN users ON logs.user_id=users.id 
-                        GROUP BY username 
-                        ORDER BY COUNT(username) '''
-            display(conn, sql)
-		# If argument is a number returns usernames that are that length
-        elif args[1].isdigit():
-            print("numberic")
-            sql = ''' SELECT month, day, time, username, ip, city, region, country FROM logs 
-                        JOIN dates ON logs.date_id = dates.id
-                        JOIN locations ON logs.location_id = locations.id
-                        JOIN users ON logs.user_id = users.id
-                        WHERE length(username) = ''' + args[1] + ''';'''
-            display(conn, sql)
-		# If argument is a username displays the month, day, time username, city, region, and country with that username
-        else:
-            sql = ''' SELECT month, day, time, username, ip, city, region, country FROM logs 
-                        JOIN dates ON logs.date_id = dates.id
-                        JOIN locations ON logs.location_id = locations.id
-                        JOIN users ON logs.user_id = users.id
-                        WHERE username = "''' + args[1] + '''";'''
-            display(conn, sql)
+        sql = ''' SELECT month, day, time, username, ip, city, region, country FROM logs
+                    JOIN dates ON logs.date_id = dates.id
+                    JOIN users ON logs.user_id = users.id 
+                    JOIN locations ON logs.location_id = locations.id '''
+        allLogins = getAll(conn, sql)
+        getBots(allLogins)
 
 def create_connection(db_file):
     try:
@@ -43,14 +24,54 @@ def create_connection(db_file):
         
     return None
 
-def display(conn, sql):
+
+def getAll(conn, sql):
     conn.text_factory = str
     cur = conn.cursor()
     cur.execute(sql)
 
-    rows = cur.fetchall()
+    return cur.fetchall()
 
-    for row in rows:
-        print(row)
+def getBots(allLogins):
+    prevRow = None
+    for row, nextRow in zip(allLogins, allLogins[1:]):
+        month = row[0]
+        nextMonth = nextRow[0]
+
+        day = row[1]
+        nextDay = nextRow[1]
+
+        time = row[2]
+        nextTime = nextRow[2]
+
+        username = row[3]
+        nextUsername = nextRow[3]
+
+        if month == nextMonth and day == nextDay and username == nextUsername:
+            if closeInTime(time, nextTime):
+                if row != prevRow:
+                    print " "
+                    print(row)
+
+                print(nextRow)
+                prevRow = nextRow
+
+def closeInTime(time, nextTime):
+        timeList = time.split(":")
+
+        hour = int(timeList[0])
+        minuite = int(timeList[1])
+
+        nextTimeList = nextTime.split(":")
+
+        nextHour = int(nextTimeList[0])
+        nextMinuite = int(nextTimeList[1])
+
+        if hour == nextHour and nextMinuite - minuite < 2:
+            return True
+        elif nextHour - 1 == hour and nextMinuite - (60 - nextMinuite) < 2:
+            return True
+        else:
+            return False
     
 main()
