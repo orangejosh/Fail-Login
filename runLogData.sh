f62c70eb7a8d7b669a0216dd185f5314c565e450
Josh <josh@Joshs-iMac.local>
2019-04-10 18:32:54 -0500

    Abstracted the runLogData.sh script

--- runLogData.sh
+++ runLogData.sh
@@ -2,18 +2,20 @@
 
 #############################################################################################
 # runLogData.sh
+# With the path to the log file as an argument
+# ex ./runLogData.sh /var/log/auth.log.1
 #
-# parshData.sh 
+# parseData.sh 
 # Filters out the failed system login attemps. Then looks up the city, region, and country from
 # the ip address logged. Then writes the month, day, time, ip, username, city, region, and
 # country to a temp log.
 #
 # logData.py
 # Goes through the temp log and inserts the data into a database (It is assumed the database
-# is all ready created).
+# is all ready created in the logData directory and is named logData.db).
 #
 # logBots.py
-# Makes the assumption that two loggins with the same username, within two minuites of each
+# Makes the assumption that two loggins with the same username, within X minuites of each
 # other are controlled by the same bot regardless of the ip address. It logs these bot
 # attacks into a table.
 #
@@ -22,19 +24,14 @@
 #############################################################################################
 
 
+cd /home/pi/logData/
+
+log_path=$1
 DATE=`date "+%m-%d"`
 
-# parseData.sh <path/to/authorization/log> <path/to/temp/log>
-/home/pi/logData/logScripts/parseData.sh /var/log/auth.log.1 /home/pi/logData/fail$DATE.log
+logScripts/parseData.sh $log_path fail$DATE.log
+python logScripts/logData.py logData.db fail$DATE.log
+python logScripts/logBots.py logData.db
 
-# python logData.py <path/to/database> <path/to/temp/log>
-python /home/pi/logData/logScripts/logData.py /home/pi/logData/logData.db /home/pi/logData/fail$DATE.log
-
-# python logBots.py <path/to/database> 
-python /home/pi/logData/logScripts/logBots.py /home/pi/logData/logData.db
-
-# python gzip <path/to/temp/log>
-gzip /home/pi/logData/fail$DATE.log
-
-# mv <path/to/temp/log> <path/to/archive/directory>
-mv /home/pi/logData/fail$DATE.log.gz /home/pi/logData/oldLogs
+gzip fail$DATE.log
+mv fail$DATE.log.gz oldLogs/
