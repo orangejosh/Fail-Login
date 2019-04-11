8608af8974d4940a7129272ab0091b9e1e4eff20
Joshua Lytle <joshua.lytle@gmail.com>
2019-04-10 20:31:53 -0500

    Updated for cron

--- runLogData.sh
+++ runLogData.sh
@@ -2,8 +2,7 @@
 
 #############################################################################################
 # runLogData.sh
-# With the path to the log file as an argument
-# ex ./runLogData.sh /var/log/auth.log.1
+# argument: path to the log file ex ./runLogData.sh /var/log/auth.log.1
 #
 # parseData.sh 
 # Filters out the failed system login attemps. Then looks up the city, region, and country from
@@ -11,20 +10,17 @@
 # country to a temp log.
 #
 # logData.py
-# Goes through the temp log and inserts the data into a database (It is assumed the database
-# is all ready created in the logData directory and is named logData.db).
+# Goes through the temp log and inserts the data into a database.
 #
 # logBots.py
-# Makes the assumption that two loggins with the same username, within X minuites of each
+# Makes the assumption that two loggins with the same username, within X minutes of each
 # other are controlled by the same bot regardless of the ip address. It logs these bot
 # attacks into a table.
-#
-# gzip and mv
-# Zips the temp log and moves it to an archive
 #############################################################################################
 
 
-cd /home/pi/logData/
+# If using cron uncomment and change this to the absolute path of the program directory.
+#cd /home/pi/logData/
 
 log_path=$1
 DATE=`date "+%m-%d"`
@@ -34,4 +30,5 @@ python logScripts/logData.py logData.db fail$DATE.log
 python logScripts/logBots.py logData.db
 
 gzip fail$DATE.log
+mkdir oldLogs
 mv fail$DATE.log.gz oldLogs/
